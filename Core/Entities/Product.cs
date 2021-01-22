5fb99a01c5e22ea0e26a054a429126d1fd6721d8
Erik Maide <maide.erik@gmail.com>
2021-01-22 22:03:27 +0200

    add generic repository

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,6 +1,6 @@
 namespace Core.Entities
 {
-    public class Product : BaseEntity
+     public class Product : BaseEntity
     {
         public string Name { get; set; }
         public string Description { get; set; }
@@ -9,7 +9,6 @@ namespace Core.Entities
         public ProductType ProductType { get; set;}
         public int ProductTypeId { get; set; }
         public ProductBrand ProductBrand { get; set;}
-
         public int ProductBrandId { get; set; }
     }
 }
